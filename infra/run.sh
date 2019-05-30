ba4caf06fe274e612869a32ec7790ba6489f0f31
reptation <david@musicalquilt.com>
2019-05-30 01:29:37 +0000

    update run.sh to use helper scripts

--- infra/run.sh
+++ infra/run.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-
+set -e
 # Assumes packer and terraform are installed
 
 # start from infra directory
 cd $(dirname "$0")
+if [ -z "${GIT_BRANCH}" ];then
+    source ./scripts/get-creds.sh
+    if [ -z "${GIT_BRANCH}" ];then
+        echo "Failed to get branchname. Exiting"
+        exit 1
+    fi
+fi
 
-pushd ./packer
-packer build webworker-dashboard.json
-popd 
+./scripts/replace-packer-ami.sh
+#pushd ./packer
+#packer build webworker-dashboard.json
+#popd 
 
-pushd ./terraform
-terraform init
-terraform apply
-popd
+./scripts/config-and-run-terraform.sh
+#pushd ./terraform
+#terraform init
+#terraform apply
+#popd
 
 echo "Deployment Complete"
 
